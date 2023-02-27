658c8189356b1372f56c2437b684289bd4b9c086
irminaj <irminaj@github.com>
2023-02-27 20:03:05 +0200

    add new Post form and add create Post functionality

---
+++ create_post.js
@@ -0,0 +1,26 @@
+const createPostForm = document.getElementById("create-post");
+
+const API_ENDPOINTS = {
+  createPost: "	https://testapi.io/api/irminaj/resource/newPosts",
+};
+
+const createPost = (url, data) => {
+  return fetch(url, {
+    method: "POST",
+    body: data,
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => console.log(err));
+};
+
+const handlePostSubmit = async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const newPost = await createPost(API_ENDPOINTS.createPost, formData);
+  e.target.reset();
+};
+
+createPostForm.addEventListener("submit", handlePostSubmit);
